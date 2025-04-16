f41f689f8d1650407defedb0190f6b6a2bbcdc3a
jczahor <jennifer.czahor@gmail.com>
2025-04-16 00:26:21 -0500

    Add files via upload
    Files for about, index, javascript, and a portfolio were added to project.

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//Future development needed
+//Let's alert the user that they should have a great day on each page
+
+
+//Step 1. Add css element to index or home page
+//Step 2. ... to about page
+//Step 3. ... to portfolio page
+
+
+//step1
+const positiveprompt = "Have a great day!"
+
+
+
+
+alert(positiveprompt + " Please hit OK to visit page~")
+
